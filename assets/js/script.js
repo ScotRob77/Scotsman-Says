fafab91fde29bf382312c52891a22f8586830973
ScotRob77 <rob.wilkes@yahoo.co.uk>
2022-07-29 11:58:14 +0000

    create startGame function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,10 @@
 let sequence = []
 let playerSequence = []
+
+const startButton = document.querySelector('.js-start');
+
+function startGame() {
+  startButton.classList.add('hidden')
+}
+
+startButton.addEventListener('click', startGame);
