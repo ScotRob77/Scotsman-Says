89a2d305f3581da9db068ce6d66b100b4d207991
ScotRob77 <rob.wilkes@yahoo.co.uk>
2022-07-29 12:27:13 +0000

    Add shadow styling to tiles

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,12 @@ let sequence = []
 let playerSequence = []
 
 const startButton = document.querySelector('.js-start');
+const info = document.querySelector('.js-info');
 
 function startGame() {
-  startButton.classList.add('hidden')
+  startButton.classList.add('hidden');
+  info.classList.remove('hidden');
+  info.textContent = 'Wait for the computer';
 }
 
 startButton.addEventListener('click', startGame);
