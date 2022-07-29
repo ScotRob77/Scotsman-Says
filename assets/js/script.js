2914744eab93357dbb6d8bc819e958ffcba3a9df
ScotRob77 <rob.wilkes@yahoo.co.uk>
2022-07-29 12:44:23 +0000

    Add activate tiles function

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,16 @@ let level = 0;
 const startButton = document.querySelector('.js-start');
 const info = document.querySelector('.js-info');
 
+function activateTile(color) {
+  const tile = document.querySelector(`[data-tile='${color}']`);
+  const sound = document.querySelector(`[data-sound='${color}']`);
+
+  tile.classList.add('activated');
+  setTimeout(() => {
+    tile.classList.remove('activated');
+  }, 300);
+}
+
 function nextStep() {
   const tiles = ['red', 'blue', 'green', 'yellow'];
   const random = tiles[Math.floor(Math.random() * tiles.length)]
@@ -16,6 +26,7 @@ function nextRound() {
   level++;
 
   const nextSequence = [...sequence]
+  nextSequence.push(nextStep());
 }
 
 function startGame() {
