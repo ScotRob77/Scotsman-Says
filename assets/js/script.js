21649180106be94e41d0fed02291a1eac45377ee
ScotRob77 <rob.wilkes@yahoo.co.uk>
2022-07-29 12:35:21 +0000

    Create next round and step functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,22 @@
 let sequence = []
 let playerSequence = []
+let level = 0;
 
 const startButton = document.querySelector('.js-start');
 const info = document.querySelector('.js-info');
+
+function nextStep() {
+  const tiles = ['red', 'blue', 'green', 'yellow'];
+  const random = tiles[Math.floor(Math.random() * tiles.length)]
+
+  return random
+}
+
+function nextRound() {
+  level++;
+
+  const nextSequence = [...sequence]
+}
 
 function startGame() {
   startButton.classList.add('hidden');
